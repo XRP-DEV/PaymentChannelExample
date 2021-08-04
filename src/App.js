544c9a8180f049fe3c84090cb62bc68e572ee6fa
Nicolai Helldorff <nicolai.helldorff@gmail.com>
2021-08-04 06:55:37 +0200

    ADDED Stuff

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Card, Button, Row, Container, Col} from "react-bootstrap";
-import {deriveKeypair} from "ripple-lib";
+import {Card, Button, Row, Container, Col, Form, Alert} from "react-bootstrap";
+import {deriveKeypair, xrpToDrops} from "ripple-lib";
+import axios from "axios";
+
 
 // Connect ---------------------------------------------------------------------
 const ripple = require('ripple-lib')
@@ -10,16 +11,26 @@ let api = new ripple.RippleAPI({server: 'wss://s.altnet.rippletest.net:51233'})
 api.connect();
 
 
+// parameters
+const payersAddress = "rP8RjzppsE34SeHD4YYZNaprKdqsGJRCk3";
+const payerPublicKey = api.deriveKeypair("sn5St2TYJSuBbanu81hH9GNUa3ZP8").publicKey;
+const publicKeyInHex = "303243463737463832343433354541423332433637353834373433464630364443333846354139423532303435324545344638303133323343423732323742333031";
+const payeeAddress = "radBdVvjUDF1VGgJo5nLvyGEexpZcWtiHu";
+//alert(payerPublicKey)
+
+// signature: 3044022064EBCAC123DF62C4BF1E405D88301B36A72F4344B6AFCC65B9DEF57ED9C5E4C202203BFF61E06948D758152D67886AE242686498D4C618FF4F807D9D806836B8482D
+
 const createPaymentChannel = async () => {
-    const pubKey = api.deriveKeypair("sn5St2TYJSuBbanu81hH9GNUa3ZP8").publicKey;
+
+    let xrp = document.getElementById("amountXRP").value;
 
     const preparedTx = await api.prepareTransaction({
-        "Account": "rP8RjzppsE34SeHD4YYZNaprKdqsGJRCk3",
+        "Account": payersAddress,
         "TransactionType": "PaymentChannelCreate",
-        "Amount": "10000000",
-        "Destination": "radBdVvjUDF1VGgJo5nLvyGEexpZcWtiHu",
-        "SettleDelay": 6000,
-        "PublicKey": pubKey
+        "Amount": api.xrpToDrops(xrp),
+        "Destination": payeeAddress,
+        "SettleDelay": 600,
+        "PublicKey": payerPublicKey
     }, {
         "maxLedgerVersionOffset": 75
     });
@@ -41,17 +52,54 @@ const createPaymentChannel = async () => {
     const result = await api.submit(tx_blob)
     console.log("Tentative result code:", result.resultCode)
     console.log("Tentative result message:", result.resultMessage)
+
+    alert("created channel")
 }
 
-const payWithPaymentChannel = async () => {
+const verifyClaim = async () => {
+
+    const channelId = document.getElementById("verifyChannelID").value;
+    const secret = document.getElementById("secret").value;
+    const amountVerifyXRPClaim = document.getElementById("amountVerifyXRPClaim").value;
+
+
+    // Simple POST request with a JSON body using fetch
+    const requestOptions = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            "method": "channel_authorize",
+            "params": [{
+                "channel_id": channelId,
+                "secret": secret,
+                "amount": api.xrpToDrops(amountVerifyXRPClaim)
+            }]
+        })
+    };
+    fetch('/', requestOptions)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("signatureStatus").innerText = JSON.stringify(data.result.signature);
+        })
+        .catch(err => console.log(err));
+
+
+};
+
+const makeClaim = async () => {
+    const amount = document.getElementById("makeClaimAmountXRPClaim").value;
+    const id = document.getElementById("makeClaimChannelID").value;
+    const signature = document.getElementById("makeClaimSignature").value;
+
 
     const preparedTx = await api.prepareTransaction({
-        "id": "channel_authorize_example_id1",
-        "command": "channel_authorize",
-        "channel_id": "7B5D9FA3695D3619CF9FE494A82A6E07F3817222EF357E3A59E70AE687A82204",
-        "seed": "sn5St2TYJSuBbanu81hH9GNUa3ZP8",
-        "key_type": "secp256k1",
-        "amount": "1000000",
+        "Account": "radBdVvjUDF1VGgJo5nLvyGEexpZcWtiHu",
+        "TransactionType": "PaymentChannelClaim",
+        "Amount": xrpToDrops(amount),
+        "Balance": xrpToDrops(amount),
+        "Channel": id,
+        "PublicKey": "02CF77F824435EAB32C67584743FF06DC38F5A9B520452EE4F801323CB7227B301",
+        "Signature": signature
     }, {
         "maxLedgerVersionOffset": 75
     });
@@ -62,8 +110,8 @@ const payWithPaymentChannel = async () => {
     console.log("Transaction expires after ledger:", maxLedgerVersion)
 
     // signing transaction
-    const signed = api.sign(preparedTx.txJSON, "sn5St2TYJSuBbanu81hH9GNUa3ZP8")
-    const txID = signed.id
+    const signed = api.sign(preparedTx.txJSON, "snnowQVoWEsS64KDvbtnrECGyFgnf")
+    const txID = signed.id;
     const tx_blob = signed.signedTransaction
     console.log("Identifying hash:", txID)
     console.log("Signed blob:", tx_blob)
@@ -72,6 +120,8 @@ const payWithPaymentChannel = async () => {
     const result = await api.submit(tx_blob)
     console.log("Tentative result code:", result.resultCode)
     console.log("Tentative result message:", result.resultMessage)
+
+    alert("Made claim. Success!")
 }
 
 function App() {
@@ -82,24 +132,60 @@ function App() {
                     <Col>
                         <Card style={{width: '30rem'}}>
                             <Card.Body>
-                                <Card.Title>rP8RjzppsE34SeHD4YYZNaprKdqsGJRCk3</Card.Title>
+                                <Card.Title>Payer: rP8RjzppsE34SeHD4YYZNaprKdqsGJRCk3</Card.Title>
                                 <Card.Text>
                                     Secret: sn5St2TYJSuBbanu81hH9GNUa3ZP8
                                 </Card.Text>
-                                <Button variant="primary" onClick={createPaymentChannel}>Create payment channel</Button>
-                                <Button variant="primary" onClick={payWithPaymentChannel}>Pay 10 drops via
-                                    Channel</Button>
+                                <Form>
+                                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                                        <Form.Label>XRP Amount</Form.Label>
+                                        <Form.Control type="number" placeholder="Amount in XRP" id={'amountXRP'}/>
+                                        <Button variant="primary" onClick={createPaymentChannel}>Create payment
+                                            channel</Button>
+                                    </Form.Group>
+                                </Form>
+
                             </Card.Body>
                         </Card>
                     </Col>
                     <Col>
                         <Card style={{width: '30rem'}}>
                             <Card.Body>
-                                <Card.Title>radBdVvjUDF1VGgJo5nLvyGEexpZcWtiHu</Card.Title>
+                                <Card.Title>Receiver: radBdVvjUDF1VGgJo5nLvyGEexpZcWtiHu</Card.Title>
                                 <Card.Text>
                                     Secret: snnowQVoWEsS64KDvbtnrECGyFgnf
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                <Form>
+                                    <Form.Label>XRP Amount</Form.Label>
+                                    <Form.Control type="number" placeholder="XRP claim" id={'makeClaimAmountXRPClaim'}/>
+                                    <Form.Label>Channel ID</Form.Label>
+                                    <Form.Control type="text" placeholder="Channel ID" id={'makeClaimChannelID'}/>
+                                    <Form.Label>Signature</Form.Label>
+                                    <Form.Control type="text" placeholder="Signature" id={'makeClaimSignature'}/>
+                                    <Button variant="primary" onClick={makeClaim}>Make claim</Button>
+                                </Form>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card style={{width: '30rem'}}>
+                            <Card.Body>
+                                <Card.Title>Verify Claim</Card.Title>
+                                <Card.Text>
+                                    <Form>
+                                        <Form.Label>XRP Amount</Form.Label>
+                                        <Form.Control type="number" placeholder="XRP claim"
+                                                      id={'amountVerifyXRPClaim'}/>
+                                        <Form.Label>Channel ID</Form.Label>
+                                        <Form.Control type="text" placeholder="Channel ID" id={'verifyChannelID'}/>
+                                        <Form.Label>Secret</Form.Label>
+                                        <Form.Control type="text" placeholder="Secret" id={'secret'}/>
+                                        <Button variant="primary" onClick={verifyClaim}>Verify claim</Button>
+                                    </Form>
+                                    <Alert>
+                                        <p id={'signatureStatus'}></p>
+                                    </Alert>
+                                </Card.Text>
                             </Card.Body>
                         </Card>
                     </Col>
